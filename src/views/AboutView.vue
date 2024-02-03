3ffccf2e0a5ef6e5219a84f4355e939b1f4f58ba
wei886-tw <lunhowl@gmail.com>
2024-02-03 13:28:03 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{data}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    }
+  },
+  mounted() {
+    this.$http.get('https://randomuser.me/api/').then((res) => {
+      this.data = res.data
+    })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
